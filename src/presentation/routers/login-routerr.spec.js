304057c80fa8f3b93cc64469100e4ff2db73424f
Carol <castrocaroline11@gmail.com>
2020-11-25 21:14:22 -0300

    add tdd

---
+++ src/presentation/routers/login-routerr.spec.js
@@ -0,0 +1,37 @@
+class LoginRouter {
+  route (httpRequest) {
+    if (!httpRequest.body.email || !httpRequest.body.password) {
+      return {
+        statusCode: 400
+      }
+    }
+  }
+}
+
+describe('Login Router', () => {
+  test('deve retornar 400 se o email nao for fornecido', () => {
+    const sut = new LoginRouter()
+
+    const httpRequest = {
+      body: {
+        password: 'senha_qualquer'
+      }
+
+    }
+    const httpResponse = sut.route(httpRequest)
+    expect(httpResponse.statusCode).toBe(400)
+  })
+
+  test('deve retornar 400 se o password nao for fornecido', () => {
+    const sut = new LoginRouter()
+
+    const httpRequest = {
+      body: {
+        email: 'email_qualquer@email.com'
+      }
+
+    }
+    const httpResponse = sut.route(httpRequest)
+    expect(httpResponse.statusCode).toBe(400)
+  })
+})
